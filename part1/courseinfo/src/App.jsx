ed65ace0b3e27d2579bfb969c1a68d89712d3e09
Uday <uday@Udays-MacBook-Air.local>
2025-09-01 21:01:11 +0530

    done course information 1.1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,46 @@
+function Header(props){
+  return(
+    <>
+    <h1>{props.course}</h1>
+    </>
+  )
+}
+
+
+function Content(props){
+  return(
+    <>
+    <p>{props.part} {props.exercise}</p>
+    </>
+  )
+}
+
+function Total(props){
+  return (
+    <>         
+    <p>Number of exercises {props.e1+ props.e2+ props.e3}</p>
+    </>
+  )
+}
+function App() {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part={part1} exercise={exercises1}/>
+      <Content part={part2} exercise={exercises2}/>
+      <Content part={part3} exercise={exercises3}/>
+      <Total e1={exercises1} e2={exercises2} e3={exercises3}/>
+    </div>
+  )
+}
+
+export {Content}
+export default App
