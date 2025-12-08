8b19c89cf9c98a0d45523ce94f3fa0eb21aeed4c
Uday <24b2729@iitb.ac.in>
2025-12-08 12:04:22 +0530

    done 2.4 course information part 9

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -5,59 +5,102 @@ import viteLogo from '/vite.svg'
 
 const Header = (props) => <h1>{props.course}</h1>
 
-const Content = (props) => (
+const Content = (props) => {
+  console.log("content props" ,props)
+  return(
   <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
+    {props.parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
   </div>
-)
+  )
+}
 
-const Part = (props) => (
+const Part = (props) => {
+  console.log("part props " , props);
+  return(
   <p>
     {props.part.name} {props.part.exercises}
   </p>
-)
+  )
+}
 
-const Course = (course) => {
-  console.log("course" , course);
-  console.log(course.course.parts[0].exercises)
-  const sum = course.course.parts.reduce((accumulator,part) => accumulator+part.exercises , 0)
+const Course = (courses) => {
+  console.log("course" , courses);
+  console.log(courses.courses[0].parts[0].exercises)
+  const sum1 = courses.courses[0].parts.reduce((accumulator,part) => accumulator+part.exercises , 0)
+  const sum2  = courses.courses[1].parts.reduce((accumulator,part) => accumulator+part.exercises , 0)
+  // const tot = courses.courses.reduce((accumulator , pre) => accumulator + pre[0].parts.exercises+pre[1].parts.exercises ,0)
   // const total = course.course.parts[0].exercises + course.course.parts[1].exercises + course.course.parts[2].exercises;
-  console.log(sum);
+  const total = courses.courses.map((courses)=> courses.parts.reduce((accumulator,part) => accumulator+part.exercises , 0))
+  const grandTotal = total.reduce((acc, n) => acc + n, 0);
+  console.log("grandTotal", grandTotal);
+  console.log("total" , total)
+  console.log(sum1 ,sum2);
   return(
-  <div>
-  <Header course={course.course.name} />
-  <Content parts={course.course.parts} />
-  <Total total={sum}/>
+  <>
+  {courses.courses.map((course,idx) => (
+  <div key={course.id}>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+     <Total total={total[idx]}/>
   </div>
+  ))}
+  </>
   )
 }
 
 const Total = (props) => <p>total of {props.total}  exercises</p>
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-      },
-    ],
-  }
+    const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   console.log("App working")
 
-  return <Course course={course} />
+  return <Course courses={courses} />
 }
 
 export default App
