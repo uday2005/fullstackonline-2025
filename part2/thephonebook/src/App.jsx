d57ea2abd95bac21485d92b203decec98bcd5424
Uday <24b2729@iitb.ac.in>
2025-12-17 05:16:34 +0530

    done 2.12 to 2.15 the phonebook

--- part2/thephonebook/src/App.jsx
+++ part2/thephonebook/src/App.jsx
@@ -1,48 +1,104 @@
 import { useState , useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import axios from 'axios'
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
+import personService from './services/persons';
+
+
 const App = () => {
   const [persons, setPersons] = useState([]) 
 
-  const hook = () => {
-    axios
-          .get("http://localhost:3001/persons")
-          .then(response => {
-            console.log("resposne fulfilled" ,response )
-            setPersons(response.data)
-          })
-
+  const fetchPersons = () => {
+        personService
+        .getAll()
+        .then(person => setPersons(person))
+          // .get("http://localhost:3001/persons")
+          // .then(response => {
+          //   console.log("resposne fulfilled" ,response )
+          //   setPersons(response.data)
+          // })
   }
-  useEffect(hook , []);
 
+
+  useEffect(fetchPersons , []);
+
+  const handleClick = (id) => {
+    
+      if (window.confirm("Do you want to delete this user")) {
+          personService
+          .deleteP(id) // return the promise to delete 
+          .then(() => {
+          setPersons(prev => prev.filter(p => p.id !== id)) // filter out the persons which is not with id it is being deletd
+                        })
+      } else {
+        return ;
+      }
+  }
+
+  
+  
+  // useEffect(deletePerson , [])
   const addPerson = (event) => {
     event.preventDefault();
-
-    const isAlready = persons.some(person => person.name === newName)
-      // we using some here as it is non-mutable  it means it does not change the array.
-      if (isAlready=== true){
-        alert(newName + " is already added to phonebook")
-        return;
-      }
-      
     const newPerson = {
       name : newName,
-      id : persons.length + 1,
+      // id : persons.length + 1,
+      // Lets remove it and lets backend assign this itself not by ours
       number : numbers,
     };
 
-    const info= [...persons,newPerson];
-    // copy the persons list and then add new person in that list.
-    setPersons(info);
-    setNewName('');
-    setNumbers('');
+    const isAlready = persons.some(person => person.name === newName) && persons.some(person => person.number === numbers)
+      // we using some here as it is non-mutable  it means it does not change the array.
+      if (isAlready=== true){
+        alert(newName + " with same number is already added to phonebook")
+        console.log("isAlready condition reached" , persons.some(person => person.name === newName) && persons.some(person => person.number === numbers))
+        return;
+      }
+      else if(persons.some(person => person.name === newName))
+        {
+          console.log("name is only same no number same " ,persons.some(person => person.name === newName),persons.find(person => person.name === newName) )
+          const sameName = persons.find(person => person.name === newName)
+          if (window.confirm(sameName.name + " is already added to the phonebook ,replace the old one with new number")) 
+          {
+          personService
+          .update(sameName.id , newPerson)
+          .then(updatedperson =>{
+              setPersons(prev => 
+                prev.map (p => (p.id === updatedperson.id) ? {...p , number : updatedperson.number}: p)
+              )
+            })
+          } 
+          else 
+            {
+            return ;
+            }
+          }
+      else{
+        
+    personService
+    .create(newPerson)
+    .then(person => {
+      // const info= [...persons,person];
+      // // copy the persons list and then add new person in that list.
+      // setPersons(info);
 
-  }
+      // This can cause the problem if multiple adds happen quickly , this can become stale.
+      setPersons(prev => prev.concat(person));
+      setNewName('');
+      setNumbers('');
+
+      
+    }) 
+      }
+      
+    
+
+    }
+
+   
+
+  
   const [newName, setNewName] = useState('')
   const [numbers , setNumbers] = useState('')
   const [namef , setNamef] = useState('')
@@ -63,7 +119,8 @@ const App = () => {
     setNamef(event.target.value);
   }
 
-  return (
+
+  return(
     <div>
       <h2>Phonebook</h2>
       <Filter onChange={handleNamef} value={namef}/>
@@ -75,7 +132,7 @@ const App = () => {
       onNumberChange={handleNumberChange}
                                         />
       <h2>Numbers</h2>
-      <Persons persons={persons} namef={namef}/>
+      <Persons persons={persons} namef={namef} onclick={handleClick}/>
     </div>
   )
 }
--- part2/thephonebook/src/components/Persons.jsx
+++ part2/thephonebook/src/components/Persons.jsx
@@ -3,7 +3,15 @@ const Persons = (props) => {
   return (
         props.persons
             .filter(person => person.name.toLowerCase().includes(props.namef.toLowerCase())) // filter expects a boolean check 
-            .map(person => <div key = {person.id}> {person.name} ----- {person.number}</div>)
+            .map(person => 
+            <div key = {person.id}> 
+            {person.name} ----- {person.number} {' '}
+            <button onClick={() => props.onclick(person.id)}> Delete </button>
+            {/* Here it should be like function not jut onclick(persion.id) as that will execuete instantely */}
+
+            </div>
+            )
+
   )
 }
 
