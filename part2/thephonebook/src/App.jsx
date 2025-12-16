5e4cf04208946ba676e3b790cdb6223d5dee19bc
Uday <24b2729@iitb.ac.in>
2025-12-16 16:30:09 +0530

    done 2.11 the phonebook

--- part2/thephonebook/src/App.jsx
+++ part2/thephonebook/src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
+import axios from 'axios'
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
 
+  const hook = () => {
+    axios
+          .get("http://localhost:3001/persons")
+          .then(response => {
+            console.log("resposne fulfilled" ,response )
+            setPersons(response.data)
+          })
+
+  }
+  useEffect(hook , []);
 
   const addPerson = (event) => {
     event.preventDefault();
